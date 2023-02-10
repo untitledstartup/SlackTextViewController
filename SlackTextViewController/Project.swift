6acad33afe420f5800004b92a2679b085e71cd12
Andrej Kolar <andrej.kolar@me.com>
2023-02-10 18:45:30 +0100

    PLAT-892: Update tuist to 2.0.0

--- SlackTextViewController/Project.swift
+++ SlackTextViewController/Project.swift
@@ -1,26 +1,35 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
 
-let project = Project(name: "SlackTextViewController",
-                      organizationName: BuildInfo.organizationName,
-                      targets: [
-                        Target(name: "SlackTextViewController",
-                               platform: .iOS,
-                               product: .framework,
-                               productName: "SlackTextViewController",
-                               bundleId: "com.slack.SlackTextViewController",
-                               deploymentTarget: BuildInfo.deploymentTarget,
-                               infoPlist: .file(path: "SlackTextViewController/Info.plist"),
-                               sources: [
-                                "../Source/**/*.m"
-                               ],
-                               headers: Headers(public: [
-                                "SlackTextViewController/*.h",
-                                "../Source/**/*.h"
-                               ]),
-                               settings: Settings(
-                                 configurations: [
-                                  .debug(name: "Debug", settings: ["EXCLUDED_ARCHS[sdk=iphonesimulator*]": "arm64"], xcconfig: "SlackTextViewController.xcconfig"),
-                                  .release(name: "Release", xcconfig: "SlackTextViewController.xcconfig")
-                                ]))
-                      ])
+let project = Project(
+    name: "SlackTextViewController",
+    organizationName: BuildInfo.organizationName,
+    targets: [
+        Target(
+            name: "SlackTextViewController",
+            platform: .iOS,
+            product: .framework,
+            productName: "SlackTextViewController",
+            bundleId: "com.slack.SlackTextViewController",
+            deploymentTarget: BuildInfo.deploymentTarget,
+            infoPlist: .file(path: "SlackTextViewController/Info.plist"),
+            sources: [
+                "../Source/**/*.m"
+            ],
+            headers: Headers(public: [
+                "SlackTextViewController/*.h",
+                "../Source/**/*.h"
+            ]),
+            settings: Settings.settings(
+                configurations: [
+                    .debug(
+                        name: "Debug",
+                        settings: ["EXCLUDED_ARCHS[sdk=iphonesimulator*]": "arm64"],
+                        xcconfig: "SlackTextViewController.xcconfig"
+                    ),
+                    .release(name: "Release", xcconfig: "SlackTextViewController.xcconfig")
+                ]
+            )
+        )
+    ]
+)
